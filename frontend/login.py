422788c839757880eae552a4c00e36ba255650d7
Alison <alison.soares@sou.ufac.br>
2025-09-28 22:38:57 -0500

    tela de login

--- frontend/login.py
+++ frontend/login.py
@@ -5,10 +5,7 @@ from ttkbootstrap.style import Style
 app = ttk.Window("LOGIN")
 app.geometry("500x500")
 style = Style(theme="superhero")
-style.colors.bg = "#5bc0de"       # fundo principal (claro)
-style.colors.inputbg = "#ffffff"  # fundo dos campos Entry
-style.colors.primary = "#5bc0de"  # azul claro para botões
-style.colors.secondary = "#3498db"
+
 
 
 
@@ -28,8 +25,8 @@ ttk.Entry(senha, show="*" ).pack(side=LEFT, fill="x", expand=True, padx=5)
 
 botao = ttk.Frame(app)
 botao.pack(pady=30, padx=10, fill="x")
+ttk.Button(botao, text="cancelar", bootstyle=DANGER).pack(side=LEFT, padx=100)
 ttk.Button(botao, text="Enviar", bootstyle=SUCCESS).pack(side=LEFT, padx=15)
-ttk.Button(botao, text="cancelar", bootstyle=DANGER).pack(side=LEFT, padx=15)
 
 
 app.mainloop()
